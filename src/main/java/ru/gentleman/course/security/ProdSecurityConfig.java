5728eb92e975cf640fddb45209bc6861f16f48b3
Egor <egormalov.2007@gmail.com>
2026-03-07 18:08:13 +0300

    GP-13: security configs added, JwtRoleConverter added

---
+++ src/main/java/ru/gentleman/course/security/ProdSecurityConfig.java
@@ -0,0 +1,33 @@
+package ru.gentleman.course.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("prod")
+@Configuration
+@RequiredArgsConstructor
+public class ProdSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new JwtRoleConverter());
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->
+                        requests.anyRequest()
+                                .authenticated()) //TODO сделать проверку на доступ к урокам
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwtConfigurer ->
+                                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        return http.build();
+    }
+}
